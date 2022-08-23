d9524402454d947bb7eaec3e255804032534df28
Aluce123 <107504512+Aluce123@users.noreply.github.com>
2022-08-23 16:40:20 -0300

    Add files via upload

---
+++ C82.js
@@ -0,0 +1,38 @@
+console.log("Meu primeiro projeto no console");
+
+            var firstName = "Rebeca";
+            var lastName = "Kapoor";
+
+firstName.concat(lastName)
+
+
+            var fullName = 2 ;
+            console.log(fullName);
+
+            var a = 10;
+            var b = 5;
+var add = a + b
+
+
+ console.log(add)
+
+            var add = 2  ;
+            console.log( );
+
+
+var sub = a - b
+
+
+ console.log(sub)
+
+            var sub =  2 ;
+            console.log( );
+
+ var multiple = a * b
+console.log(multiple)
+
+            var multiple =  2 ;
+            console.log();
+
+            var divide = a / b ;
+            console.log(divide);
